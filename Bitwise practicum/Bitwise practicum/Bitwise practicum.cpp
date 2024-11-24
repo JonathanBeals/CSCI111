f2ed2d9881fa6093471b368e69822df531a40363
JonathanBeals <jrbeals@mavs.coloradomesa.edu>
2024-11-24 13:32:30 -0700

    Adding files

---
+++ Bitwise practicum/Bitwise practicum/Bitwise practicum.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+int main() {
+	//Exercise 1
+
+	/*
+	int a = 12;
+	int b = 10;
+
+	int result = a & b;
+	std::cout << result;
+	*/
+
+	//Exercise 2
+	/*
+	int a = 14;
+	int b = 9;
+
+	int result = a | b;
+	std::cout << result;
+	*/
+
+	//Exercise 3
+	/*
+	int a = 15;
+	int b = 6;
+
+	int result = a ^ b;
+	std::cout << result;
+	*/
+
+	//Exercise 4
+	/*
+	int a = 10;
+
+	int result = ~a;
+
+	std::cout << result;
+	*/
+
+	//Exercise 5
+	/*
+	int a = 7;
+	int b = 3;
+
+	int result = a << b;
+	std::cout << result;
+	*/
+
+	//Exercise 6
+	/*
+	int a = 16;
+	int b = 2;
+
+	int result = a >> b;
+	std::cout << result;
+	*/
+
+	//Exercise 7
+	/*
+	int num = 45;
+	if (num & 1) std::cout << "Odd";
+	else std::cout << "Even";
+	*/
+
+	//Exercise 8
+	/*
+	int num = 9;
+	int pos = 1;
+	num = num ^ (1 << pos);
+	std::cout << num;
+	*/
+
+	//Exercise 9
+
+	int num = 12;
+	int pos = 2;
+	num = num | (1 << pos);
+	std::cout << num;
+
+
+
+
+
+
+
+	return 0;
+}
